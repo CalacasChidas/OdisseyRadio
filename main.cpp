2d9b9a4be48d1a414305d751b1207d8f323677f1
CalacasChidas <ale-jimenez@estudiantec.cr>
2023-09-19 23:03:47 -0600

    Lógica para cargar la música (aún no carga en el TreeView

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ struct Song {
     std::string album;
 };
 
-GtkWidget *window, *PruebaArduino, *Exit;
+GtkWidget *window, *PruebaArduino, *Exit, *TreeView;
 GtkLabel *memoryLabel;
 GtkProgressBar *progressMemory;
 
@@ -22,6 +22,7 @@ std::vector<Song> loadLibraryFromCSV(const std::string& csvFilePath) {
 
     // Abre el archivo CSV
     std::ifstream csvFile(csvFilePath);
+    std::cout << "Done!\n";
     if (!csvFile.is_open()) {
         std::cerr << "Error al abrir el archivo CSV" << std::endl;
         return library;
@@ -29,6 +30,7 @@ std::vector<Song> loadLibraryFromCSV(const std::string& csvFilePath) {
 
     std::string line;
     while (std::getline(csvFile, line)) {
+        //std::cout<<"Im doing somehting\n";
         std::istringstream iss(line);
         std::string title, artist, album;
         if (std::getline(iss, title, ',') &&
@@ -50,13 +52,6 @@ void pruebaArduino(){
 double getMemoryUsage(){
     std::ifstream meminfo("/proc/meminfo");
     std::string line;
-
-    // Ruta al archivo CSV de canciones
-    std::string csvFilePath = "/home/aleprominecraft/Documents/github/OdisseyRadio/fma-master";
-    std::vector<Song> library = loadLibraryFromCSV(csvFilePath);
-    for (const Song& song : library) {
-        std::cout << "Título: " << song.title << ", Artista: " << song.artist << ", Álbum: " << song.album << std::endl;
-    }
 
     double total_mem = 0.0;
     double available_mem = 0.0;
@@ -116,6 +111,23 @@ int main(int argc, char *argv[]) {
     GtkBuilder *builder; // GTK Builder variable
     gtk_init(&argc, &argv); // Start GTK
 
+    //Crear un GtkListStore para almacenar los datos de las canciones
+    GtkListStore *listStore = gtk_list_store_new(3, G_TYPE_STRING, G_TYPE_STRING, G_TYPE_STRING);
+
+    std::string csvFilePath = "/home/aleprominecraft/fma/data/fma_metadata/tracks.csv";
+    std::vector<Song> library = loadLibraryFromCSV(csvFilePath);
+    /*
+    for (const Song& song : library) {
+        std::cout << "Título: " << song.title << ", Artista: " << song.artist << ", Álbum: " << song.album << std::endl;
+    }
+     */
+    for (const Song& song : library) {
+        GtkTreeIter iter;
+        gtk_list_store_append(listStore, &iter);
+        gtk_list_store_set(listStore, &iter, 0, song.title.c_str(), 1, song.artist.c_str(), 2, song.album.c_str(), -1);
+    }
+
+
     builder = gtk_builder_new(); // Create GTK UI Builder
     gtk_builder_add_from_file(builder, "/home/aleprominecraft/Documents/github/OdisseyRadio/Odissey.glade", NULL); // Load our UI file
 
@@ -125,6 +137,18 @@ int main(int argc, char *argv[]) {
     progressMemory = GTK_PROGRESS_BAR(gtk_builder_get_object(builder, "progressMemory"));
     Exit = GTK_WIDGET(gtk_builder_get_object(builder, "Exit"));
     PruebaArduino = GTK_WIDGET(gtk_builder_get_object(builder, "PruebaArduino"));
+    TreeView = gtk_tree_view_new_with_model(GTK_TREE_MODEL(listStore));
+
+    for (int i = 0; i < 3; ++i) {
+        GtkCellRenderer* renderer = gtk_cell_renderer_text_new();
+        GtkTreeViewColumn* column = gtk_tree_view_column_new_with_attributes(
+                (i == 0) ? "Título" : (i == 1) ? "Artista" : "Álbum",
+                renderer,
+                "text", i,
+                NULL
+        );
+        gtk_tree_view_append_column(GTK_TREE_VIEW(TreeView), column);
+    }
 
     //Declaración de llamadas de funciones
     g_signal_connect(Exit, "clicked", G_CALLBACK(exit_app), NULL);
@@ -134,6 +158,7 @@ int main(int argc, char *argv[]) {
     g_object_unref(builder);
 
     g_timeout_add_seconds(1, reinterpret_cast<GSourceFunc>(updateLabel), memoryLabel);
+
 
     gtk_widget_show_all(window); // Show our window
     gtk_main(); // Run our program
